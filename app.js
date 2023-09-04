ac6c3d8a3d413f90c8bbae0f87cf28f0720edb3e
Abdulla0233 Valiyev <vliyevabdulla+demo@gmail.com>
2023-09-04 10:31:24 +0000

    input padding

--- app.js
+++ app.js
@@ -2,25 +2,37 @@
 $(document).ready(function () {
 
 
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url =  `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=e9de91a45ac9470dc1bdf5abb260bfec&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("#myWeather").val("");
+                console.log(data)
+            }
+            myweather()
+        }
 
+    });
 
-
-
-
-    
-    
-    $("#btn").click(function (e) { 
-        e.preventDefault()
-
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=e9de91a45ac9470dc1bdf5abb260bfec`
-        async function myWeather() {
+    $("#btn").click(function (e) {
+        e.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=e9de91a45ac9470dc1bdf5abb260bfec&units=metric`
+        async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
-            // $("#city").html(data.name);
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myWeather").val("");
             console.log(data)
         }
-        myWeather()
-        
+        myweather()
     });
+
 
 });
